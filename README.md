00fee94f928112f0f03abc3900f787d4a3e601c4
Dave Trost <davetrost@gmail.com>
2019-09-17 15:15:47 -0700

    array shift delete function

--- README.md
+++ README.md
@@ -8,4 +8,8 @@ I took the aproach to reverse the array 'in-place'. My solution runs in O(n/2) t
 
 ## [Array shift](challenges/arrayShift/array-shift.js)
 
-The challenge is to take in an array and the value to be added. Without utilizing any of the built-in methods available to your language, return an array with the new value added at the middle index.
+The challenge is to take in an array and a value to be added. The function returns an array with the new value added at the middle index.
+
+A second function removes an element from the middle index and shifts other elements in the array to fill the new gap.
+
+I took the aproach to shift the array 'in-place'. My solution runs in O(n/2) time (linear) and uses O(1) space (constant).
--- __test__/array-shift.test.js
+++ __test__/array-shift.test.js
@@ -1,11 +1,15 @@
 'use strict';
 
-let insertShiftArray = require('../challenges/arrayShift/array-shift');
+let {insertShiftArray, deleteShiftArray} = require('../challenges/arrayShift/array-shift');
 
-describe('array shift', () => {
+describe('array insert shift', () => {
 
   it('shifts 5 into the middle position', () => {
     expect(insertShiftArray([2,4,6,8], 5)).toEqual([2,4,5,6,8]);
+  });
+
+  it('shifts 5 into the middle position of an array with an odd number of elements', () => {
+    expect(insertShiftArray([2,3,4,6,8], 5)).toEqual([2,3,4,5,6,8]);
   });
 
   it('shifts into an empty array', () => {
@@ -15,6 +19,27 @@ describe('array shift', () => {
   it('shifts the array in-place', () => {
     const arrayInput = [1, 2, 3];
     expect(insertShiftArray(arrayInput, 99)).toBe(arrayInput);
+  });
+
+});
+
+describe('array delete shift', () => {
+
+  it('removes 5 from the middle position', () => {
+    expect(deleteShiftArray([2,4,5,6,8])).toEqual([2,4,6,8]);
+  });
+
+  it('removes 5 from the middle position of an array with an even couunt of elements', () => {
+    expect(deleteShiftArray([2,4,5,6])).toEqual([2,4,6]);
+  });
+
+  it('does not change an empty array', () => {
+    expect(deleteShiftArray([])).toEqual([]);
+  });
+
+  it('modifies the array in-place', () => {
+    const arrayInput = [1, 2, 3, 99];
+    expect(deleteShiftArray(arrayInput)).toBe(arrayInput);
   });
 
 });
