5758cd3d44421a9447db794feeca8a697521aa4e
Dave Trost <davetrost@gmail.com>
2019-09-16 22:39:42 -0700

    refactored array reversal

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -1,9 +1,7 @@
 function arrayReverse(arr) {
   for(let i = 0; i < (arr.length - 1) / 2; i++) {
-    const temp = arr[i];
-    arr[i] = arr[arr.length - 1 - i];
-    arr[arr.length - 1 - i] = temp;
-    console.log(arr);
+    const mirrorI = arr.length - 1 - i;
+    [arr[i], arr[mirrorI]] = [arr[mirrorI], arr[i]];
   }
   return arr;
 }
