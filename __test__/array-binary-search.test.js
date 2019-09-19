b981b6cfb89281b91d8ff1fcbc0f29dcd1f90440
Dave Trost <davetrost@gmail.com>
2019-09-19 01:55:40 -0700

    refactor binary search

--- __test__/array-binary-search.test.js
+++ __test__/array-binary-search.test.js
@@ -5,11 +5,15 @@ let { binarySearch } = require('../challenges/arrayBinarySearch/array-binary-sea
 describe('array binary search', () => {
 
   it('finds an search key in the array', () => {
+    expect(binarySearch([4,8,15,16,23,42], 4)).toBe(0);
     expect(binarySearch([4,8,15,16,23,42], 15)).toBe(2);
+    expect(binarySearch([4,8,15,16,23,42], 42)).toBe(5);
   });
 
   it('returns "-1" when the search key does not exist', () => {
-    expect(binarySearch([11,22,33,44,55,66,77], 90)).toBe(-1);
+    expect(binarySearch([11,22,33,44,55,66], 90)).toBe(-1);
+    expect(binarySearch([11,22,33,44,55,66], 0)).toBe(-1);
+    expect(binarySearch([11,22,33,44,55,66], 40)).toBe(-1);
   });
 
 });
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -5,26 +5,19 @@
  * @returns {*} the index of an element in the array that matches the search key, or -1 if the search key does not exist in the array
  * @function binarySearch
  */
-function binarySearch(sortedArr, ele) {
-  const middleIndex = Math.floor(sortedArr.length / 2);
+function binarySearch(sortedArr, ele, startIndex, endIndex) {
+  if(startIndex === undefined) startIndex = 0;
+  if(endIndex === undefined) endIndex = sortedArr.length - 1;
+  
+  const arrLength = endIndex - startIndex + 1;
+  const middleIndex = startIndex + Math.floor(arrLength / 2);
+
+  if(arrLength < 1) return -1;
   if(ele === sortedArr[middleIndex]) return middleIndex;
-  if(sortedArr.length > 2 && ele > sortedArr[middleIndex]) {
-    const newArr = []; 
-    for(let i = middleIndex + 1; i < sortedArr.length; i++) {
-      newArr[i - middleIndex - 1] = sortedArr[i];
-    }
-    const newSearch = binarySearch(newArr, ele);
-    if(newSearch === -1) return -1;
-    return middleIndex + 1 + newSearch;
-  }
-  if(sortedArr.length > 1 && ele < sortedArr[middleIndex]) {
-    const newArr = [];
-    for(let i = 0; i < middleIndex; i++) {
-      newArr[i] = sortedArr[i];
-    }
-    return binarySearch(newArr, ele);
-  }
-  return -1;
+
+  if(ele > sortedArr[middleIndex]) startIndex = middleIndex + 1;
+  if(ele < sortedArr[middleIndex]) endIndex = middleIndex - 1;
+  return binarySearch(sortedArr, ele, startIndex, endIndex);
 }
 
 module.exports = { binarySearch };
