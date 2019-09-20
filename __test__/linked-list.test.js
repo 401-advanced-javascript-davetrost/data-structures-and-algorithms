c9f3bd8d25a810022fc8ddc1f767106ae77ad362
Dave Trost <davetrost@gmail.com>
2019-09-20 11:59:58 -0700

    linked list implementation passing tests

--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -1,10 +1,48 @@
 'use strict';
 
 let { LinkedList } = require('../challenges/linkedList/linked-list');
+let linkedList;
 
-describe.skip('thing', () => {
+const value1 = 'one';
+const value2 = 'two';
+const value3 = 'three';
 
-  it('does something', () => {
+describe('Linked List', () => {
+
+  it('Can successfully instantiate an empty linked list', () => {
+    linkedList = new LinkedList();
+    expect(linkedList.head).toBe(null);
+  });
+  
+  it('Can properly insert into the linked list', () => {
+    linkedList.insert(value1);
+    expect(linkedList.head.value).toBe(value1);
+  });
+  
+  it('The head property will properly point to the first node in the linked list', () => {
+    linkedList.insert(value2);
+    expect(linkedList.head.value).toBe(value2);
+  });
+  
+  it('Can properly insert multiple nodes into the linked list', () => {
+    linkedList.insert(value3);
+    expect(linkedList.head.value).toBe(value3);
+    expect(linkedList.head.next.value).toBe(value2);
+    expect(linkedList.head.next.next.value).toBe(value1);
+  });    
+  
+  it('Will return true when finding a value within the linked list that exists', () => {
+    expect(linkedList.includes(value1)).toBe(true);
+    expect(linkedList.includes(value2)).toBe(true);
+    expect(linkedList.includes(value3)).toBe(true);
+  });
+      
+  it('Will return false when searching for a value in the linked list that does not exist', () => {
+    expect(linkedList.includes('non-existent-value')).toBe(false);
+  });
+      
+  it('Can properly return a collection of all the values that exist in the linked list', () => {
+    expect(linkedList.toString()).toBe([value3, value2, value1].join(', '));
   });
 
 });
--- challenges/linkedList/linked-list.js
+++ challenges/linkedList/linked-list.js
@@ -1,3 +1,58 @@
+const ListNode = require('./list-node');
+
 /**
  * Linked List Class implementation
  */
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  
+  /**
+   * insert an element into the list at the head position
+   * @param {*} ele
+   * @function insert
+   */
+  insert(ele) {
+    const newHead = new ListNode(ele);
+    newHead.next = this.head;
+    this.head = newHead;
+    return newHead;
+  }
+
+  /**
+   * determine if an element exists in the list (at any position)
+   * @param {*} ele
+   * @returns {boolean}
+   * @function includes
+   */
+  includes(ele) {
+    let current = this.head;
+    while(current !== null) {
+      if(current.value === ele) return true;
+      current = current.next;
+    }
+    return false;
+  }
+
+  
+  /**
+   * return a string representing all the values in the Linked List
+   * @returns {string}
+   * @function toString
+   */
+  toString() {
+    if(this.head === null) return '';
+    let allValues = this.head.value;
+    let current = this.head.next;
+    while(current !== null) {
+      allValues += ', ' + current.value;
+      current = current.next;
+    }
+    return allValues;
+  }
+
+}
+
+module.exports = { LinkedList };
