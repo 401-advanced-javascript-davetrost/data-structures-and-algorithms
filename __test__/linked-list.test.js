ff6d3c7d78acf3df80838d79cc0769456320f650
Dave Trost <davetrost@gmail.com>
2019-09-25 11:49:37 -0700

    skip unecessary tests

--- __test__/linked-list.test.js
+++ __test__/linked-list.test.js
@@ -12,7 +12,7 @@ const value3 = 'three';
 const value4 = 'four';
 const value5 = 'five';
 
-describe('Linked List', () => {
+describe.skip('Linked List', () => {
 
   it('Can successfully instantiate an empty linked list', () => {
     linkedList = new LinkedList();
