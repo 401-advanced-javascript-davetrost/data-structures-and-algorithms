67de522b940ce64443a800f93c32053e5579487e
Dave Trost <davetrost@gmail.com>
2019-09-30 17:44:16 -0700

    queue implemented with stacks

--- __test__/queue-with-stacks.test.js
+++ __test__/queue-with-stacks.test.js
@@ -9,39 +9,37 @@ const value3 = 'three';
 
 describe('Pseudo-Queue Data Structure', () => {
 
-  // it('Can successfully instantiate an empty queue', () => {
-  //   queue = new PseudoQueue();
-  //   expect(queue.front).toBe(null);
-  //   expect(queue.length).toBe(0);
-  // });
+  it('Can successfully instantiate an empty queue', () => {
+    queue = new PseudoQueue();
+    expect(queue.inStack.top).toBe(null);
+    expect(queue.outStack.top).toBe(null);
+  });
   
-  // it('Can successfully enqueue onto a queue', () => {
-  //   queue.enqueue(value1);
-  //   expect(queue.front.value).toBe(value1);
-  //   expect(queue.length).toBe(1);
-  // });
+  it('Can successfully enqueue onto a queue', () => {
+    queue.enqueue(value1);
+    expect(queue.inStack.top.value).toBe(value1);
+  });
   
-  // it('Can successfully enqueue multiple values onto a queue', () => {
-  //   queue.enqueue(value2);
-  //   queue.enqueue(value3);
-  //   expect(queue.front.value).toBe(value1);
-  //   expect(queue.toString()).toBe(`${value1}, ${value2}, ${value3}`);
-  //   expect(queue.length).toBe(3);
-  // });
+  it('Can successfully enqueue multiple values onto a queue', () => {
+    queue.enqueue(value2);
+    expect(queue.inStack.top.value).toBe(value2);
+    queue.enqueue(value3);
+    expect(queue.inStack.top.value).toBe(value3);
+    expect(queue.toString()).toBe(`${value1}, ${value2}, ${value3}`);
+  });
   
-  // it('Can successfully dequeue from the queue', () => {
-  //   expect(queue.dequeue()).toBe(value1);
-  //   expect(queue.toString()).toBe(`${value2}, ${value3}`);
-  //   expect(queue.length).toBe(2);
-  // });
+  it('Can successfully dequeue from the queue', () => {
+    expect(queue.dequeue()).toBe(value1);
+    expect(queue.toString()).toBe(`${value2}, ${value3}`);
+  });
   
-  // it('Can successfully empty a queue after multiple dequeues', () => {
-  //   expect(queue.dequeue()).toBe(value2);
-  //   expect(queue.toString()).toBe(`${value3}`);
-  //   expect(queue.length).toBe(1);
-  //   expect(queue.dequeue()).toBe(value3);
-  //   expect(queue.front).toBe(null);
-  //   expect(queue.length).toBe(0);
-  // });
+  it('Can successfully empty a queue after multiple dequeues', () => {
+    expect(queue.dequeue()).toBe(value2);
+    expect(queue.toString()).toBe(`${value3}`);
+    expect(queue.dequeue()).toBe(value3);
+    expect(queue.toString()).toBe(``);
+    expect(queue.inStack.top).toBe(null);
+    expect(queue.outStack.top).toBe(null);
+  });
   
 });
